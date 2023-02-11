1554d27f2b7bf37abb5d164e174d6f986622207f
Robertkq <robertvuia06@gmail.com>
2023-02-11 03:33:12 +0200

    Fixed small makefile warning

--- tests/main.cpp
+++ tests/main.cpp
@@ -12,7 +12,7 @@ int main()
     Color color = Red;
     auto name = magic_enum::enum_name(color);
     std::cout << name << "\n";
-    std::cout << magic_enum::enum_count<Color>() << '\n';
+    std::cout << magic_enum::enum_count<Color>() + 20 << '\n';
     std::cout << sizeof(std::vector<std::string>) << " " << sizeof(std::string) << '\n';
     return 0;
     
