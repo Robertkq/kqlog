9dadf88a34b76451cf2887a479a1e3c619fc19c0
Robert <robertvuia06@gmail.com>
2023-02-25 00:23:40 +0200

    Console color works on Win/Linux, File color disabled on both

--- include/kqlog/common.h
+++ include/kqlog/common.h
@@ -1,12 +1,7 @@
 #ifndef KQLOG_COMMON_H
 #define KQLOG_COMMON_H
-#if defined (_WIN32)
-    #define _OS_COPY "copy "
-#elif defined (__linux__)
-    #define _OS_COPY "cp "
-#endif
-#define FMT_HEADER_ONLY
 
+#define FMT_HEADER_ONLY
 #include <iostream>
 #include <cstdint>
 #include <string>
@@ -20,5 +15,31 @@
 #include <unordered_map>
 #include <algorithm>
 #include <ctime>
+
+namespace kq
+{
+    enum class default_symbols : uint8_t
+    {
+        INFO,
+        DEBUG,
+        WARNING,
+        ERROR,
+        CRITICAL
+    };
+}
+
+#define KQINFO kq::default_symbols::INFO
+#define KQDEBUG kq::default_symbols::DEBUG
+#define KQWARNING kq::default_symbols::WARNING
+#define KQERROR kq::default_symbols::ERROR
+#define KQCRITICAL kq::default_symbols::CRITICAL
+
+#if defined (_WIN32)
+    #define _OS_COPY "copy "
+    #include <windows.h>
+#elif defined (__linux__)
+    #define _OS_COPY "cp "
+    #include <unistd.h>
+#endif
 
 #endif
--- include/kqlog/logger.h
+++ include/kqlog/logger.h
@@ -5,20 +5,7 @@
 
 namespace kq
 {
-    enum class default_symbols : uint8_t
-    {
-        INFO,
-        DEBUG,
-        WARNING,
-        ERROR,
-        CRITICAL
-    };
-
-#define KQINFO kq::default_symbols::INFO
-#define KQDEBUG kq::default_symbols::DEBUG
-#define KQWARNING kq::default_symbols::WARNING
-#define KQERROR kq::default_symbols::ERROR
-#define KQCRITICAL kq::default_symbols::CRITICAL
+    
 
     enum class time_zone : uint8_t
     {
@@ -51,7 +38,7 @@ namespace kq
     {
         public:
             using encoding_type = C;
-            using string_type   = std::conditional_t<std::is_same_v<C, char>, std::string, std::wstring>;
+            using string_type   = std::conditional_t<std::is_same_v<encoding_type, char>, std::string, std::wstring>;
 
         public:
             time_info(const std::tm* time);
@@ -64,24 +51,24 @@ namespace kq
             string_type str_from_int(I convert) const noexcept;
 
         public:
-            int sec;
-            int min;
-            int hour;
-            int mday;
-            int mon;
-            int year;
-            int wday;
+            int m_sec;
+            int m_min;
+            int m_hour;
+            int m_mday;
+            int m_mon;
+            int m_year;
+            int m_wday;
     };
 
     template<typename C>
     time_info<C>::time_info(const std::tm* tm)
-        : time_info(std::tm(*tm))
+        :   time_info(std::tm(*tm))
     {}
 
     template<typename C>
     time_info<C>::time_info(const std::tm& tm)
-        : sec(tm.tm_sec), min(tm.tm_min), hour(tm.tm_hour), mday(tm.tm_mday), mon(tm.tm_mon + 1),
-         year(tm.tm_year + 1900), wday(tm.tm_wday)
+        :   m_sec(tm.tm_sec), m_min(tm.tm_min), m_hour(tm.tm_hour), m_mday(tm.tm_mday), m_mon(tm.tm_mon + 1),
+            m_year(tm.tm_year + 1900), m_wday(tm.tm_wday)
     {}
 
     template<typename C>
@@ -110,29 +97,29 @@ namespace kq
     template<typename C>
     typename time_info<C>::string_type time_info<C>::pretty_month(bool abbr) const
     {
-        if(mon == 1) return abbr ? "Jan" : "January";
-        else if(mon == 2) return abbr ? "Feb" : "February";
-        else if(mon == 3) return abbr ? "Mar" : "March";
-        else if(mon == 4) return abbr ? "Apr" : "April";
-        else if(mon == 5) return "May";
-        else if(mon == 6) return abbr ? "Jun" : "June";
-        else if(mon == 7) return abbr ? "Jul" : "July";
-        else if(mon == 8) return abbr ? "Aug" : "August";
-        else if(mon == 9) return abbr ? "Sep" : "September";
-        else if(mon == 10) return abbr ? "Oct" : "October";
-        else if(mon == 11) return abbr ? "Nov" : " November";
+        if(m_mon == 1) return abbr ? "Jan" : "January";
+        else if(m_mon == 2) return abbr ? "Feb" : "February";
+        else if(m_mon == 3) return abbr ? "Mar" : "March";
+        else if(m_mon == 4) return abbr ? "Apr" : "April";
+        else if(m_mon == 5) return "May";
+        else if(m_mon == 6) return abbr ? "Jun" : "June";
+        else if(m_mon == 7) return abbr ? "Jul" : "July";
+        else if(m_mon == 8) return abbr ? "Aug" : "August";
+        else if(m_mon == 9) return abbr ? "Sep" : "September";
+        else if(m_mon == 10) return abbr ? "Oct" : "October";
+        else if(m_mon == 11) return abbr ? "Nov" : " November";
         else return abbr ? "Dec" : "December";
     }
     
     template<typename C>
     typename time_info<C>::string_type time_info<C>::pretty_day(bool abbr) const
     {
-        if(wday == 0) return abbr ? "Sun" : "Sunday";
-        else if(wday == 1) return abbr ? "Mon" : "Monday";
-        else if(wday == 2) return abbr ? "Tue" : "Tuesday";
-        else if(wday == 3) return abbr ? "Wed" : "Wednesday";
-        else if(wday == 4) return abbr ? "Thu" : "Thursday";
-        else if(wday == 5) return abbr ? "Fri" : "Friday";
+        if(m_wday == 0) return abbr ? "Sun" : "Sunday";
+        else if(m_wday == 1) return abbr ? "Mon" : "Monday";
+        else if(m_wday == 2) return abbr ? "Tue" : "Tuesday";
+        else if(m_wday == 3) return abbr ? "Wed" : "Wednesday";
+        else if(m_wday == 4) return abbr ? "Thu" : "Thursday";
+        else if(m_wday == 5) return abbr ? "Fri" : "Friday";
         else return abbr ? "Sat" : "Saturday";
     }
 
@@ -149,13 +136,16 @@ namespace kq
             template<typename... Args>
             log(const string_type& fmt, Args&&... args);
             
-            string_type convert(string_type arg) const;
+            string_type convert(string_type arg, bool console) const;
 
-            string_type& fmt() { return m_fmt; }
-            const string_type& fmt() const { return m_fmt; } 
+            string_type& file_fmt() { return m_file_fmt; }
+            const string_type& file_fmt() const { return m_file_fmt; }
+            string_type& console_fmt() { return m_console_fmt; }
+            const string_type& console_fmt() const { return m_console_fmt; } 
 
         private:
-            string_type m_fmt; 
+            string_type m_file_fmt;
+            string_type m_console_fmt;
 
             static std::unordered_map<C, string_type> s_flags;   
     };
@@ -169,11 +159,12 @@ namespace kq
     template<typename T, typename C>
     template<typename... Args>
     log<T, C>::log(const string_type& fmt, Args&&... args)
-        : m_fmt(fmt::format(convert(fmt), std::forward<Args>(args)...))
+        :   m_file_fmt(fmt::format(fmt::runtime(convert(fmt, false)), std::forward<Args>(args)...)), 
+            m_console_fmt(fmt::format(fmt::runtime(convert(fmt, true)), std::forward<Args>(args)...))
     {}
 
     template<typename T, typename C>
-    typename log<T, C>::string_type log<T, C>::convert(string_type arg) const
+    typename log<T, C>::string_type log<T, C>::convert(string_type arg, bool console) const
     {
         char aux;
         for(int i=0; i<arg.size(); ++i)
@@ -190,7 +181,8 @@ namespace kq
                         arg.erase(arg.begin()+i);
                         arg.erase(arg.begin()+i);
                         arg.erase(arg.begin()+i);
-                        arg.insert(i, s_flags[aux]);
+                        if(console)
+                            arg.insert(i, s_flags[aux]);
                     }
                 }
             }
@@ -215,15 +207,11 @@ namespace kq
         ~logger();
 
         logger& operator=(const logger& other) = delete;
-        logger& operator=(logger&& other); // add me
-        const string_type& get_realpattern() const
-        {
-            return m_realpattern;
-        }
+        logger& operator=(logger&& other);
         void set_pattern(const string_type& new_pattern = "[{%Y}-{%M}-{%D} {%H}:{%N}:{%S}] [{%T}] [{%F}@{%L}] {%V}\n");
         void set_time(time_zone tz) noexcept;
         void set_filter(const std::vector<event_type>& toFilter, bool filterMode = true);
-        void set_silent(bool sz = true);
+        void set_silent(bool silent = true);
         void backup();
 
         template<typename... Args>
@@ -237,7 +225,8 @@ namespace kq
         string_type             m_filename;
         string_type             m_directory;
         string_type             m_logpattern;
-        string_type             m_realpattern;
+        string_type             m_console_pattern;
+        string_type             m_file_pattern;
         std::vector<event_type> m_tofilter;
         bool                    m_filterin; // 1 means filter IN, 0 means filter OUT
         time_zone               m_timezone;
@@ -247,7 +236,7 @@ namespace kq
 
     private:
         time_info get_time() const;
-        string_type convert_pattern(string_type);
+        string_type convert_pattern(string_type, bool) const;
     };
 
     //K, R, G, y, E, g, C, W
@@ -263,34 +252,37 @@ namespace kq
     template<typename T, typename C>
     logger<T, C>::logger(const string_type& filename, const string_type& directory, time_zone tz)
         :   m_mutex(), m_file(directory + filename, ofstream_type::out), m_filename(filename), m_directory(directory),
-            m_logpattern("[{%Y}-{%M}-{%D} {%H}:{%N}:{%S}] [{%T}] [{%F}@{%L}] {%V}" ), m_realpattern("[{2}-{3}-{6} {9}:{10}:{11}] [{1}] [{14}@{13}] {0}"),
-            m_tofilter(), m_filterin(), m_timezone(tz), m_silent()
-    {}
+            m_logpattern("[{%Y}-{%M}-{%D} {%H}:{%N}:{%S}] [{%T}] [{%F}@{%L}] {%V}" ), m_console_pattern("[{2}-{3}-{6} {9}:{10}:{11}] [{1}] [{14}@{13}] {0}"),
+            m_file_pattern("[{2}-{3}-{6} {9}:{10}:{11}] [{1}] [{14}@{13}] {0}"), m_tofilter(), m_filterin(), m_timezone(tz), m_silent()
+    {
+        system(""); // necessary sometimes to let the console know we are going to use ANSI escape color codes
+    }
 
     template<typename T, typename C>
     logger<T, C>::logger(logger&& other) noexcept
         :   m_mutex(), m_file(std::move(other.m_file)),
             m_filename(std::move(other.m_filename)), m_directory(std::move(other.m_directory)),
-            m_logpattern(std::move(other.m_logpattern)), m_realpattern(std::move(other.m_realpattern)),
-            m_tofilter(std::move(other.m_tofilter)), m_filterin(other.m_filterin),m_timezone(other.m_timezone),
-            m_silent(other.m_silent)
+            m_logpattern(std::move(other.m_logpattern)), m_console_pattern(std::move(other.m_console_pattern)),
+            m_file_pattern(std::move(other.m_file_pattern)), m_tofilter(std::move(other.m_tofilter)), m_filterin(other.m_filterin),
+            m_timezone(other.m_timezone), m_silent(other.m_silent)
     {
         std::unique_lock lock(other.m_mutex);
-        //other.m_file.close();
+        system("");
     }
 
     template<typename T, typename C>
     logger<T, C>::~logger()
     {
-        std::unique_lock lock(m_mutex);
+        //std::unique_lock lock(m_mutex);
     }
 
     template<typename T, typename C>
     void logger<T, C>::set_pattern(const string_type& new_pattern)
     {
         std::unique_lock lock(m_mutex);
-        m_logpattern    = new_pattern;
-        m_realpattern   = convert_pattern(m_logpattern);
+        m_logpattern        = new_pattern;
+        m_console_pattern   = convert_pattern(m_logpattern, true);
+        m_file_pattern      = convert_pattern(m_logpattern, false);
     }
 
     template<typename T, typename C>
@@ -327,12 +319,12 @@ namespace kq
         }
         m_file.close();
         time_info ti        = get_time();
-        string_type year    = ti.str_from_int(ti.year);
-        string_type month   = ti.str_from_int(ti.mon);
-        string_type day     = ti.str_from_int(ti.mday);
-        string_type hour    = ti.str_from_int(ti.hour);
-        string_type minute  = ti.str_from_int(ti.min);
-        string_type second  = ti.str_from_int(ti.sec);
+        string_type year    = ti.str_from_int(ti.m_year);
+        string_type month   = ti.str_from_int(ti.m_mon);
+        string_type day     = ti.str_from_int(ti.m_mday);
+        string_type hour    = ti.str_from_int(ti.m_hour);
+        string_type minute  = ti.str_from_int(ti.m_min);
+        string_type second  = ti.str_from_int(ti.m_sec);
         string_type backreturn = year +"-" + month + "-" + day + "_" + hour + ":" + minute + ":" + second;
         system((_OS_COPY + m_directory + m_filename + string_type (" ") + m_directory
          + string_type("backup/") + backreturn + string_type("-") + m_filename ).c_str());
@@ -346,45 +338,46 @@ namespace kq
     {
         std::unique_lock lock(m_mutex);
         time_info ti = get_time();
-        const string_type& message      = msg.fmt();                    // flag: %V -> 0 
+        const string_type& console_message  = msg.console_fmt();        // flag: %V -> 0 
+        const string_type& file_message     = msg.file_fmt();           // for file
         std::string_view type           = magic_enum::enum_name(_type); // flag: %T -> 1
-        string_type years               = ti.str_from_int(ti.year);     // flag: %Y -> 2
-        string_type months              = ti.str_from_int(ti.mon);      // flag: %M -> 3
+        string_type years               = ti.str_from_int(ti.m_year);     // flag: %Y -> 2
+        string_type months              = ti.str_from_int(ti.m_mon);      // flag: %M -> 3
         string_type pretty_full_month   = ti.pretty_month(false);       // flag: %m -> 4
         string_type pretty_abbr_month   = ti.pretty_month(true);        // flag: %b -> 5
-        string_type days                = ti.str_from_int(ti.mday);     // flag: %D -> 6
+        string_type days                = ti.str_from_int(ti.m_mday);     // flag: %D -> 6
         string_type pretty_full_day     = ti.pretty_day(false);         // flag: %d -> 7
         string_type pretty_abbr_day     = ti.pretty_day(true);          // flag: %B -> 8
-        string_type hours               = ti.str_from_int(ti.hour);     // flag: %H -> 9
-        string_type minutes             = ti.str_from_int(ti.min);      // flag: %N -> 10
-        string_type seconds             = ti.str_from_int(ti.sec);      // flag: %S -> 11
+        string_type hours               = ti.str_from_int(ti.m_hour);     // flag: %H -> 9
+        string_type minutes             = ti.str_from_int(ti.m_min);      // flag: %N -> 10
+        string_type seconds             = ti.str_from_int(ti.m_sec);      // flag: %S -> 11
         string_type thread              = ti.str_from_int(std::hash<std::thread::id>{}(std::this_thread::get_id())); // flag: %t -> 12
         string_type line                = ti.str_from_int(_line_);      // flag: %L -> 13
         string_type function            = _func_;                       // flag: %F -> 14
         string_type source              = _file_;                       // flag: %s -> 15
 
-        m_file << fmt::format(m_realpattern, message, type, years,
+        m_file << fmt::format(fmt::runtime(m_file_pattern), file_message, type, years,
              months, pretty_full_month, pretty_abbr_month, days,
              pretty_full_day, pretty_abbr_day, hours, minutes,
-             seconds, thread, line, function, source) << "\033[0m";
+             seconds, thread, line, function, source);
 
         // Check if the event_type has to be filtered out/in
         if(m_tofilter.size() > 0)
         {
             auto it = std::find(m_tofilter.begin(), m_tofilter.end(), _type);
-           if(m_filterin)
-           {
-                if(it == m_tofilter.end())
-                    return;
-           }
-           else
-           {
-                if(it != m_tofilter.end())
-                    return;
-           }
+            if(m_filterin)
+            {
+                    if(it == m_tofilter.end())
+                        return;
+            }
+            else
+            {
+                    if(it != m_tofilter.end())
+                        return;
+            }
         }
         if(!m_silent)
-        {   std::cout << fmt::format(m_realpattern, message, type, years,
+        {   std::cout << fmt::format(fmt::runtime(m_console_pattern), console_message, type, years,
                 months, pretty_full_month, pretty_abbr_month, days,
                 pretty_full_day, pretty_abbr_day, hours, minutes,
                 seconds, thread, line, function, source) << "\033[0m";
@@ -404,7 +397,7 @@ namespace kq
     }
 
     template<typename T, typename C>
-    typename logger<T, C>::string_type logger<T, C>::convert_pattern(string_type logpattern)
+    typename logger<T, C>::string_type logger<T, C>::convert_pattern(string_type logpattern, bool console) const
     {
         char aux;
         for(int i=0; i<logpattern.size(); ++i)
@@ -422,7 +415,8 @@ namespace kq
                         logpattern.erase(logpattern.begin()+i);
                         logpattern.erase(logpattern.begin()+i);
                         logpattern.erase(logpattern.begin()+i);
-                        logpattern.insert(i, s_flags[aux]);
+                        if(console) // for file_pattern we do not add any ANSI escape color codes
+                            logpattern.insert(i, s_flags[aux]);
                     }
                     else
                     {
