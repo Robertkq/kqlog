affcb90ce2cee06edf47cade3085054971a435b4
Robert <robertvuia06@gmail.com>
2023-02-25 01:13:24 +0200

    The rainbow usage example

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,37 +1,20 @@
 #include "../include/kqlog.h"
 
-kq::logger Logger("logs.txt", "output/");
 
-struct Workload
-{
-    Workload() : my_id(_id++) { Start(); }
-    void Start() 
-    { 
-        Logger.out(kq::default_symbols::INFO, {"{%y}{} {%R}Started\n", my_id});
-        Run(); 
-    }
-    void Run() 
-    { 
-        Logger.out(kq::default_symbols::DEBUG, {"{} Running\n", my_id});
-        Stop(); 
-    }
-    void Stop() 
-    {
-        Logger.out(kq::default_symbols::CRITICAL, {"{} Stopped\n", my_id});
-    };
 
-    int my_id;
-    static int _id;
-};
 
-int Workload::_id = 0;
 
 int main()
 {
-    Logger.set_pattern("[{%Y}-{%M}-{%D}{%C} {%H}:{%N}:{%S}] [{%T}] [{%F}@{%L}] {%V}");
+    kq::logger Logger("logs.txt", "output/");
     for(int i = 0; i < 10; ++i)
     {
-            Workload wl;
+        Logger.set_pattern("{%R}[{%Y}-{%M}-{%D}{%y} {%H}:{%N}:{%S}] {%G}[{%T}] {%E}[{%F}@{%L}] {%V}\n");
+        Logger.out(KQINFO, {"{%C} {0:5} {%g}START", i + 10});
+        Logger.set_pattern("{%y}[{%Y}-{%M}-{%D}{%G} {%H}:{%N}:{%S}] {%C}[{%T}] [{%F}@{%L}] {%V}\n");
+        Logger.out(KQDEBUG, {"{%g} {0:5} {%R}CONTINUE", i + 10});
+        Logger.set_pattern("{%G}[{%Y}-{%M}-{%D}{%C} {%H}:{%N}:{%S}] {%E}[{%T}] [{%F}@{%L}] {%V}\n");
+        Logger.out(KQCRITICAL, {"{%R} {0:5} {%E}END", i + 10});
     }
     
     return 0;
